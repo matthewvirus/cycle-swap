d8d98fd637a5f0338877dd06be7707fb8388051e
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-04-30 09:44:18 +0300

    passed 5 of 7

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,30 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
-
+        try {
+            int buf = array[array.length-1];
+            System.arraycopy(array, 0, array, 1, array.length-1);
+            array[0] = buf;
+        }
+        catch (ArrayIndexOutOfBoundsException exception) {
+            array = array;
+        }
     }
 
     static void cycleSwap(int[] array, int shift) {
-
+        try {
+            int[] buf = new int[array.length];
+            int iteration = 1;
+            while (iteration > 0) {
+                System.arraycopy(array, 0, buf, shift, array.length-shift);
+                System.arraycopy(array, array.length-shift, buf, 0, shift);
+                break;
+            }
+            array = buf;
+        }
+        catch (ArrayIndexOutOfBoundsException exception) {
+            exception.printStackTrace();
+            array = array;
+        }
     }
 }
